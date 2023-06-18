8591f35194394add1f6b7ab1d406b126529695ed
JaimeAdele <jaime.zuspann@gmail.com>
2023-06-18 14:13:56 -0700

    set up buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const buttonLetters = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
 
   return (
-    <>
-      
-    </>
+    <div className='bg-lightBackground border-accent border-4 w-[60vw] h-[40vh]'>
+      <div className='w-[60%] h-full p-8 grid grid-cols-3 gap-4'>
+        {buttonLetters.map(letter => (
+          <div className='bg-primary flex justify-center items-center rounded-md drop-shadow-bold font-russoOne'>{letter}</div>
+        ))}
+      </div>
+      <div></div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: 'grey',
+        // secondary: '#A288A6',
+        accent: 'orange',
+        lightBackground: '#b3b3b3',
+        darkBackground: '#8d8d8d'
+      },
+      fontFamily: {
+        russoOne: ['Russo One', 'sans-serif']
+      },
+      dropShadow: {
+        'bold': '3px 3px 3px black'
+      }
+    }
   },
   plugins: []
 };
